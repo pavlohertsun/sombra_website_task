b5ea31045165d7fcc0d219feed58c1d1c889b3af
pavlo_hertsun <hpavlo140904@gmail.com>
2023-12-06 22:30:14 +0200

    initial commit, basic html and styles added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="main_container">
+    <div class="text_container">
+      Welcome to Sigma Software website
+      <br>Read more about our
+      <a href="#" @click="scrollToDiv">team</a>
+    </div>
+  </div>
+  <div class="target_div">
+    <h2 class="text_styles">
+      Our team
+    </h2>
+    <div class="team_container">
+
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<script>
+export default {
+  methods: {
+    scrollToDiv() {
+      const targetDiv = document.querySelector('.target_div');
+      targetDiv.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+}
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.main_container {
+  height: 1080px;
+  width: 1920px;
   text-align: center;
-  color: #2c3e50;
+  background: url('@/assets/homepageimage1.png') no-repeat;
+  background-size: cover;
+  background-position: center;
 }
 
-nav {
-  padding: 30px;
+.text_container {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 32px;
+  font-weight: 400;
+  color: #d9d9d9;
+  text-align: right;
+  padding-top: 15%;
+  padding-right: 25%;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.target_div {
+  height: 500px;
+  width: 100%;
+  background-color: #282828;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.target_div h2{
+  text-align: center;
+}
+.text_styles{
+  font-family: 'Montserrat', sans-serif;
+  font-size: 32px;
+  font-weight: 400;
+  color: #d9d9d9;
 }
 </style>
---
+++ src/components/MainPageImageComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="image-container ">
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "ImageComponent"
+}
+</script>
+
+<style scoped>
+
+.image-container {
+  position: relative;
+  overflow: hidden;
+}
+#img1{
+  background-color: #282828;
+}
+</style>
